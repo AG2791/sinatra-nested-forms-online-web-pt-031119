97e062303e44167f5d6298e92b9e34fab49c526d
AJ Guiles <ashiguiles@gmail.com>
2019-06-27 01:25:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,12 +7,14 @@ module FormsLab
       get '/' do
       erb :root
     end
+    
     get '/new' do
       erb :'pirates/new'
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
+      
+      @pirate = Pirates.new(params[:pirate])
       params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
