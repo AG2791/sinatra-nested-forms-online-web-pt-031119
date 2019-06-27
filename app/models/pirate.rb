5f56ff56d33c8ac8806a541259457f794a1ade84
AJ Guiles <ashiguiles@gmail.com>
2019-06-27 01:34:55 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -14,3 +14,20 @@
 #     Pirates
 #   end
 # end
+
+class Pirate
+  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+end
